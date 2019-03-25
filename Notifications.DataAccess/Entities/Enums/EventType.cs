094684c9546def95ab9ba123b2e8eb2964ae7a4b
Cristian Harsu <harsuc@gmail.com>
2019-03-25 12:48:59 +0000

    Add project files.

---
+++ Notifications.DataAccess/Entities/Enums/EventType.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Notifications.DataAccess.Entities.Enums {
+   public enum EventType {
+      Unknown,
+      AppointmentCancelled
+   }
+}
